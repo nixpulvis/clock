de69e26be549cedc21dfbb71cd25be5b45a46f69
Nathan Lilienthal <nathan@nixpulvis.com>
2017-09-17 00:46:53 -0600

    Make a simple program to set the time.

---
+++ set.c
@@ -0,0 +1,13 @@
+#include <ds1307.h>
+#include <max7221.h>
+
+int main(void) {
+  ds1307_init(I2C_SCL_100KHZ);
+  struct tm now;
+  now.tm_hour = 12;
+  now.tm_min = 46;
+  now.tm_sec = 0;
+  ds1307_set_time(now);
+  for (;;) {}
+  return 0;
+}
